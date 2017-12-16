fa6de5684c2e4146b709cdad2087636c2869cf15
Ryan Riddell <Ryanmriddell@gmail.com>
2017-12-15 20:44:53 -0800

    initial commit

---
+++ apps/courses/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from . import views           # This line is new!
+urlpatterns = [
+    url(r'^$', views.index),
+    url(r'^courses/create$', views.create),
+    url(r'^courses/(?P<course_id>\d+)/confirm$', views.confirm),
+    url(r'^courses/(?P<course_id>\d+)/delete$', views.delete)
+  ]
